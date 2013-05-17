8dd261501a62dc644d841a71c0d9066e375d11ff
Ben Breedlove <benbreedlove@gmail.com>
2013-05-17 16:36:47 -0700

    refactor start on create shapes. make superclass start usable

--- node_shape_types.js
+++ node_shape_types.js
@@ -16,11 +16,37 @@ NodeShapeType.basic.prototype.init = function(nodes, options, app) {
     return this;
 };
 NodeShapeType.basic.prototype.start = function(callback) {
-    if (callback) {
-        callback(this);
-    }
-    return;
+    var deferreds = [];
+    var self = this;
+    this.elements = 
+        this.do_sizing_transform(
+            this.do_removing_transform(
+                this.create_element(this.nodes)
+            )
+            .style("fill", function(d){
+                return d.color;
+            })
+            .style("opacity", .6)
+                .attr("class", function(d) {
+                    return d.class;
+                })
+                .transition()
+                .duration(1000)
+        )
+        .each('start', function() {
+            this.deferred = $.Deferred(); 
+            deferreds.push( this.deferred );
+        })
+        .each('end', function() { 
+            this.deferred.resolve();
+        });
+    jQuery.when.apply(null, deferreds).done(callback);
+    return this;
 };
+
+NodeShapeType.basic.prototype.create_element = function(nodes) {
+    return nodes.append(this.element_type);
+}
 
 NodeShapeType.basic.prototype.resize = function(callback) {
     var self = this;
@@ -74,46 +100,10 @@ NodeShapeType.basic.prototype.stop = function(callback) {
 
 
 NodeShapeType.circles = function(nodes, options, app) {
+    this.element_type = 'circle';
     return this.init(nodes, options, app);
 };
 NodeShapeType.circles.prototype = new NodeShapeType.basic;
-
-NodeShapeType.circles.prototype.start = function(callback) {
-    var deferreds = [];
-    var self = this;
-    this.elements = this.nodes
-        .append('circle')
-        .attr("r", 0)
-    .style("fill", function(d){
-        return d.color;
-    })
-    .style("opacity", .6)
-        .attr("class", function(d) {
-            return d.class;
-        })
-        .transition()
-        .duration(1000)
-        .attr('r', function(d) {
-            if ( d[self.app.active_size_type] ) {
-                return self.app
-                    .size_types_desired[self.app.active_size_type]
-                    .scale(
-                        d[self.app.active_size_type]
-                    );
-            } else {
-                return 0;
-            }
-        })
-        .each('start', function() {
-            this.deferred = $.Deferred(); 
-            deferreds.push( this.deferred );
-        })
-        .each('end', function() { 
-            this.deferred.resolve();
-        })
-    jQuery.when.apply(null, deferreds).done(callback);
-}
-
 
 NodeShapeType.circles.prototype.do_sizing_transform = function(transition_element) {
     var self = this;
@@ -136,61 +126,10 @@ NodeShapeType.circles.prototype.do_removing_transform = function(transition_elem
 
 
 NodeShapeType.rectangles = function(nodes, options, app) {
+    this.element_type = 'rect';
     return this.init(nodes, options, app);
 };
 NodeShapeType.rectangles.prototype = new NodeShapeType.basic;
-
-NodeShapeType.rectangles.prototype.start = function(callback) {
-    var self = this;
-    var deferreds = [];
-
-    this.elements = this.nodes
-        .append('rect')
-        .attr("width", 0)
-        .attr("height", 0)
-        .style("fill", function(d){
-            return d.color;
-        })
-        .style("opacity", .6)
-        .attr("class", function(d) {
-                return d.class;
-        })
-        .transition()
-        .duration(1000)
-        .attr('height', function(d) {
-            if ( d[self.app.active_size_type] ) {
-                return self.app
-                    .size_types_desired[self.app.active_size_type]
-                    .scale(
-                        d[self.app.active_size_type]
-                    );
-            } else {
-                return 0;
-            }
-        })
-        .attr('width', function(d) {
-            if ( d[self.app.active_size_type] ) {
-                return self.app
-                    .size_types_desired[self.app.active_size_type]
-                    .scale(
-                        d[self.app.active_size_type]
-                    );
-            } else {
-                return 0;
-            }
-        })
-        .each('start', function() {
-            this.deferred = $.Deferred(); 
-            deferreds.push( this.deferred );
-        })
-        .each('end', function() { 
-            this.deferred.resolve();
-        })
-        ;
-
-    jQuery.when.apply(null, deferreds).done(callback);
-
-}
 
 NodeShapeType.rectangles.prototype.do_sizing_transform = function(transition_element) {
     var self = this;
